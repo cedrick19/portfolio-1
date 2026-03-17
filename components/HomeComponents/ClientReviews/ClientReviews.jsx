6114fa2ae312b34eb4f4f3e41ede09c667316c85
Cedrick <44035739+cedrick19@users.noreply.github.com>
2026-03-18 00:31:31 +0800

    Improve portfolio best practices and build reliability

--- components/HomeComponents/ClientReviews/ClientReviews.jsx
+++ components/HomeComponents/ClientReviews/ClientReviews.jsx
@@ -4,7 +4,7 @@ import { useQuery } from "react-query";
 import ParagraphSkeleton from "../../Common/ParagraphSkeleton";
 
 const ClientReviews = () => {
-  const { isLoading, error, data } = useQuery("review", () =>
+  const { isLoading, data } = useQuery("review", () =>
     axios
       .get("api/review")
       .then(({ data }) => data)
@@ -18,10 +18,13 @@ const ClientReviews = () => {
       </div>
       <div className="overflow-x-auto w-full grid  justify-items-center grid-flow-col gap-4 px-2 md:px-8 pt-2 pb-4">
         {isLoading
-          ? [1, 2, 3, 4, 5].map(() => (
-              <ParagraphSkeleton className="w-80 md:w-96 h-full p-4 md:p-8" />
+          ? [1, 2, 3, 4, 5].map((item) => (
+              <ParagraphSkeleton
+                key={`reviews-skeleton-${item}`}
+                className="w-80 md:w-96 h-full p-4 md:p-8"
+              />
             ))
-          : data?.map((data, key) => <ReviewCard key={key} data={data} />)}
+          : data?.map((review) => <ReviewCard key={review.id} data={review} />)}
       </div>
     </>
   );
